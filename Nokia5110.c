e1e2db9eb2b198c025265b26a12b3bbb2e24ca58
rithma <rithers@gmail.com>
2016-06-03 12:01:50 -0700

    Update Nokia5110.c

--- Nokia5110.c
+++ Nokia5110.c
@@ -12,32 +12,7 @@
 // Nathan Seidle
 // http://dlnmh9ip6v2uc.cloudfront.net/datasheets/LCD/Monochrome/Nokia_5110_Example.pde
 
-/* This example accompanies the book
-   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
- Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
 
-// Blue Nokia 5110
-// ---------------
-// Signal        (Nokia 5110) LaunchPad pin
-// Reset         (RST, pin 1) connected to PA7
-// SSI0Fss       (CE,  pin 2) connected to PA3
-// Data/Command  (DC,  pin 3) connected to PA6
-// SSI0Tx        (Din, pin 4) connected to PA5
-// SSI0Clk       (Clk, pin 5) connected to PA2
-// 3.3V          (Vcc, pin 6) power
-// back light    (BL,  pin 7) not connected, consists of 4 white LEDs which draw ~80mA total
-// Ground        (Gnd, pin 8) ground
 
 // Red SparkFun Nokia 5110 (LCD-10168)
 // -----------------------------------
